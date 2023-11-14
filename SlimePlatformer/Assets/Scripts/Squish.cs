318c5821baf26ba96d6d2cf3acfba2f8d75180f7
brody-brody <brody.silva@mymail.champlain.edu>
2023-11-13 22:32:46 -0500

    Final Build Before Upload
    Added squish mechanic implementation
    Known velocity bug after squish has not been fixed
    Slightly changed map for better squish usage
    Stage colors and camera implemented

--- SlimePlatformer/Assets/Scripts/Squish.cs
+++ SlimePlatformer/Assets/Scripts/Squish.cs
@@ -5,12 +5,73 @@ using UnityEngine;
 public class Squish : MonoBehaviour
 {
     public GameObject player;
-    
-    
+    bool isLeft;
+
+    private UpdatedPlayerMovement playerMovement;
+
     private void OnTriggerEnter2D(Collider2D other) 
 	{
-        Debug.Log("Tunnel trigger");
-        player.transform.localScale = new Vector3(1, 0.5f, 1);
-        player.transform.position += new Vector3(0, -0.25f, 0);
+        if (player.transform.position.y > 9.6)
+        {}
+        else
+        {
+            if (player.transform.position.x < transform.position.x)
+            {
+                isLeft = true;
+            }
+            else
+            {
+                isLeft = false;
+            }
+            playerMovement = player.GetComponent<UpdatedPlayerMovement>();
+            if (other.transform != player.transform) 
+                return;
+            playerMovement.enabled = false;
+            player.transform.localScale = new Vector3(1, 0.5f, 1);
+            if (isLeft)
+            {
+                if (player.transform.position.y > 9.25)
+                {
+                    player.transform.position += new Vector3(0, -0.2515f, 0);
+                }
+                else
+                {
+                    player.transform.position += new Vector3(0, 0.2515f, 0);
+                }
+            }
+            else
+            {
+                player.transform.position += new Vector3(0, 0.2515f, 0);
+            }
+            StartCoroutine(moveThru());
+            Invoke(nameof(ResetPM), 1.1f);
+        }
     }
+
+    IEnumerator moveThru()
+    {
+        float elapsedTime = 0;
+        float waitTime = 1f;
+        Vector3 targetPos = player.transform.position;
+        Vector3 startPos = player.transform.position;
+        if (isLeft)
+        {
+            targetPos = player.transform.position + new Vector3(6f, 0, 0);
+        }
+        else
+        {
+            targetPos = player.transform.position + new Vector3(-6f, 0, 0);
+        }
+        while (elapsedTime < waitTime)
+        {
+            player.transform.position = Vector3.Lerp(startPos, targetPos, (elapsedTime / waitTime));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        player.transform.position = targetPos;
+        player.transform.localScale = new Vector3(1, 1, 1);
+        yield return null;
+    }
+    void ResetPM() => playerMovement.enabled = true;
 }

affbaa17e8343772f8a6017f6f0f4ddf410c07ce
brody-brody <brody.silva@mymail.champlain.edu>
2023-11-16 00:40:41 -0500

    - Added finish flag - Added coins with collectible trigger - Known squish bug if you jump into the trigger

--- SlimePlatformer/Assets/Scripts/Squish.cs
+++ SlimePlatformer/Assets/Scripts/Squish.cs
@@ -6,42 +6,50 @@ public class Squish : MonoBehaviour
 {
     public GameObject player;
     bool isLeft;
+    bool resetY;
 
+    // obtain reference of player movement script for later use
     private UpdatedPlayerMovement playerMovement;
-
     private void Start() {
         playerMovement = player.GetComponent<UpdatedPlayerMovement>();
     }
 
+    // Trigger runs on collision with tunnel, tests if youre on the left or the right of the tunnel, and runs code based off of that
     private void OnTriggerEnter2D(Collider2D other) 
 	{
-
-        isLeft = (player.transform.position.x < transform.position.x);
-
-        if (other.transform != player.transform) 
-            return;
-
-        playerMovement.enabled = false;
-        player.transform.localScale = new Vector3(1, 0.5f, 1);
-
-        if (isLeft)
+        if ((transform.position.y + 0.5) > player.transform.position.y && player.transform.position.y > (transform.position.y - 0.5))
         {
-            if (player.transform.position.y < transform.position.y)
+            isLeft = (player.transform.position.x < transform.position.x);
+
+            if (other.transform != player.transform) 
+                return;
+
+            // disable player movement and squish oyu
+            playerMovement.enabled = false;
+            player.transform.localScale = new Vector3(1, 0.5f, 1);
+
+            if (isLeft)
             {
-                player.transform.position += new Vector3(0, -0.2515f, 0);
+                // if you are not above the tunnel
+                if (player.transform.position.y < transform.position.y)
+                {
+                    player.transform.position += new Vector3(0, -0.2515f, 0);
+                    resetY = true;
+                }
+                else
+                {
+                    player.transform.position += new Vector3(0, 0.2515f, 0);
+                    resetY = false;
+                }
             }
             else
             {
-                player.transform.position += new Vector3(0, 0.2515f, 0);
+                player.transform.position += new Vector3(0, -0.2515f, 0);
             }
-        }
-        else
-        {
-            player.transform.position += new Vector3(0, -0.2515f, 0);
-        }
 
-        StartCoroutine(moveThru());
-        Invoke(nameof(ResetPM), 1.1f);
+            StartCoroutine(moveThru());
+            Invoke(nameof(ResetPM), 1.1f);
+        }
     }
 
     IEnumerator moveThru()
@@ -67,6 +75,10 @@ public class Squish : MonoBehaviour
         }
 
         player.transform.position = targetPos;
+        
+        if (resetY) player.transform.position += new Vector3(0, 0.2515f, 0);
+        else player.transform.position += new Vector3(0, -0.2515f, 0);
+
         player.transform.localScale = new Vector3(1, 1, 1);
         yield return null;
     }

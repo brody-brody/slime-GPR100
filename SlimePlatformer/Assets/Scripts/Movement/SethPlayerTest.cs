bb00b71034abee7f4a7c6c1e969262694938f274
sridds <seth.riddensdale@mymail.champlain.edu>
2023-11-28 19:28:13 -0500

    Added jump to new movement system
    Works like a charm

--- SlimePlatformer/Assets/Scripts/Movement/SethPlayerTest.cs
+++ SlimePlatformer/Assets/Scripts/Movement/SethPlayerTest.cs
@@ -7,9 +7,15 @@ public class SethPlayerTest : MonoBehaviour
     [Header("Movement")]
     [SerializeField] private float moveSpeed = 5.0f;
     [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private float gravityMultiplier = 9.81f;
+
+    [Header("Gravity Forces")]
+    [SerializeField] private float gravityMultiplier = 9.806f;
     [SerializeField] private float stickToNormalForce = 15.0f;
-    [SerializeField] private float jumpForce = 5.0f;
+
+    [Header("Jump")]
+    [SerializeField] private float sideJumpForce = 5.0f;
+    [SerializeField] private float jumpUpForce = 5.0f;
+    [SerializeField] private float jumpBufferTime = 0.2f;
 
     private Vector2 currentNormal = Vector2.up;
     private Rigidbody2D rb;
@@ -18,8 +24,12 @@ public class SethPlayerTest : MonoBehaviour
     private Vector2 vel;
 
     private Vector2 currentGravity;
+    private bool isGrounded = false;
     private bool wantsToLeaveGround = false;
+    private bool jumpAttempted = false;
     private bool jumpFlag = false;
+
+    private float jumpBufferTimer;
 
     private void Awake()
     {
@@ -31,27 +41,47 @@ public class SethPlayerTest : MonoBehaviour
         // store horizontal input into the xInput variable
         xInput = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            wantsToLeaveGround = true;
-        }
+        TryQueueJump();
 
-        Ray ray = new Ray(transform.position, -currentNormal);
-        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction.normalized, 0.25f, groundLayer);
-
-        Debug.DrawRay(ray.origin, ray.direction.normalized * 0.25f, Color.red);
+        //Ray ray = new Ray(transform.position, -currentNormal);
+        //Debug.DrawRay(ray.origin, ray.direction * 3f, Color.cyan);
 
         // set current gravity based on whether or the not the player wants to leave the ground
 
-        if(wantsToLeaveGround || jumpFlag) {
-            currentGravity = new Vector2(0.0f, -gravityMultiplier);
+        if(!isGrounded || jumpFlag) {
+            currentGravity = new Vector2(0.0f, 0.0f);
+            rb.gravityScale = gravityMultiplier;
         }
-        else {
-            currentGravity = -currentNormal * stickToNormalForce;
+
+        if(isGrounded && !jumpFlag)
+        {
+            currentGravity = -currentNormal.normalized * stickToNormalForce;
+            rb.gravityScale = 0.0f;
         }
         
 
         // set the velocity
-        vel = new(Vector2.Perpendicular(currentNormal.normalized).x * -xInput * moveSpeed, Vector2.Perpendicular(currentNormal.normalized).y * -xInput * moveSpeed);
+        vel = new(Vector2.Perpendicular(currentNormal).x * -xInput * moveSpeed, Vector2.Perpendicular(currentNormal).y * -xInput * moveSpeed);
+    }
+
+    private void TryQueueJump()
+    {
+        // Reset counter when space is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpBufferTimer = jumpBufferTime;
+        }
+        // Countdown the jump buffer when not pressing space
+        else
+        {
+            jumpBufferTimer -= Time.deltaTime;
+        }
+
+        if (jumpBufferTimer > 0 && isGrounded)
+        {
+            wantsToLeaveGround = true;
+            jumpBufferTimer = 0;
+        }
     }
 
     private void FixedUpdate()
@@ -62,18 +92,40 @@ public class SethPlayerTest : MonoBehaviour
             wantsToLeaveGround = false;
 
             jumpFlag = true;
-            Invoke(nameof(ResetJumpFlag), 0.15f);
+            Invoke(nameof(ResetJumpFlag), 0.06f);
 
-            rb.velocity = new Vector2(currentNormal.x * jumpForce, currentNormal.y * jumpForce);
+            rb.velocity = new Vector2(rb.velocity.x, 0.0f);
+
+            // as long as the player isnt on the ceiling
+            if(currentNormal.y != -1)
+                rb.AddForce(Vector2.up * jumpUpForce, ForceMode2D.Impulse);
+
+            rb.AddForce(Vector2.right * currentNormal.x * sideJumpForce, ForceMode2D.Impulse);
+            //rb.velocity = new Vector2(currentNormal.x * jumpForce, currentNormal.y * jumpForce);
         }
-
-        rb.velocity = vel;
+        else if(!jumpFlag && isGrounded)
+        {
+            rb.velocity = vel;
+        }
     }
 
     private void ResetJumpFlag() => jumpFlag = false;
 
     private void OnCollisionStay2D(Collision2D collision)
     {
+        isGrounded = true;
+
         currentNormal = collision.contacts[0].normal;
+
+        // change the normal to the other contact
+        if(collision.contactCount > 1)
+        {
+            currentNormal = collision.contacts[1].normal;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
     }
 }

3635a53a5d014a92b98316628265d55e6bac211b
Newusername3115 <christopher.eichert@mymail.champlain.edu>
2023-12-01 16:11:06 -0500

    Conflicts Fixed

--- SlimePlatformer/Assets/Scripts/Level Design/Level Complete/SceneLoader.cs
+++ SlimePlatformer/Assets/Scripts/Level Design/Level Complete/SceneLoader.cs
@@ -5,8 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadScene(string sceneName)
+    public delegate void LoadScene(int scene);
+    static public LoadScene loadScene;
+
+    /// <summary>
+    ///Loads Scene passed through. Can be called with SceneLoader.loadScene?.Invoke(scene_number);
+    /// </summary>
+    /// <param name="scene"></param>
+    public void PlayScene(int scene)
     {
-        SceneManager.LoadScene(sceneName);
+        //Load passed through scene number
+        SceneManager.LoadScene(scene);
+    }
+
+    //To Prevent the delegate from being called twice
+    private void OnEnable()
+    {
+        loadScene += PlayScene;
+    }
+    private void OnDisable()
+    {
+        loadScene -= PlayScene;
     }
 }

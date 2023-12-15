af1fb5b167c21db331360298ca24b2fa1d4a91c5
sridds <seth.riddensdale@mymail.champlain.edu>
2023-12-14 21:35:11 -0500

    Air Control Fixed

--- SlimePlatformer/Assets/Animations/SethPlayerTest.cs
+++ SlimePlatformer/Assets/Animations/SethPlayerTest.cs
@@ -85,6 +85,8 @@ public class SethPlayerTest : MonoBehaviour
     private float jumpBufferTimer;
     private float lastInput;
 
+    private bool allowAirControl = false;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -190,11 +192,21 @@ public class SethPlayerTest : MonoBehaviour
         {
             //if (Mathf.Sign(rb.velocity.x) != Mathf.Sign(xInput) || Mathf.Abs(rb.velocity.x) < 0.1f)
             //{
-                //Debug.Log("Aircontrol baby T-T UWU WUUWUW OWO ");
-                //if (rb.velocity.magnitude > maxAirVelocity)
-                    rb.AddForce(Vector2.right * airControl * xInput, ForceMode2D.Force);
+            //Debug.Log("Aircontrol baby T-T UWU WUUWUW OWO ");
+            //if (rb.velocity.magnitude > maxAirVelocity)
+
+            Debug.Log("Sign of Vel: " + Mathf.Sign(rb.velocity.x) + " Sign of X INPUT: " + Mathf.Sign(xInput));
+
+            if(Mathf.Sign(rb.velocity.x) != Mathf.Sign(xInput))
+            {
+                allowAirControl = true;
+            }
+
+            Vector2 desiredVel = Vector2.right * airControl * xInput;
+            // slight air control
+            if(allowAirControl) rb.AddForce(desiredVel - rb.velocity, ForceMode2D.Force);
             //}
-            
+
         }
     }
 
@@ -259,8 +271,9 @@ public class SethPlayerTest : MonoBehaviour
             return;
         }
 
+        allowAirControl = false;
         // stop the magma particles if the players grounded
-        if(isGrounded && !jumpFlag) magmaParticles.Stop();
+        if (isGrounded && !jumpFlag) magmaParticles.Stop();
 
         isGrounded = true;
 
